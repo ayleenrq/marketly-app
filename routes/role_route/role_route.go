adc124adae08d56737f2eb0625697ef126112b43
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2026-03-06 22:56:46 +0700

    add role route

--- routes/role_route/role_route.go
+++ routes/role_route/role_route.go
@@ -4,6 +4,7 @@ import (
 	rolehandler "marketly-app/internal/handlers/role_handler"
 	rolerepository "marketly-app/internal/repositories/role_repository"
 	roleservice "marketly-app/internal/services/role_service"
+	middleware "marketly-app/internal/middlewares"
 
 	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
@@ -14,9 +15,9 @@ func RoleRoutes(e *echo.Group, db *gorm.DB) {
 	roleService := roleservice.NewRoleServiceImpl(roleRepo)
 	roleHandler := rolehandler.NewRoleHandler(roleService)
 
-	e.POST("/create", roleHandler.CreateRole)
+	e.POST("/create", roleHandler.CreateRole, middleware.AllowRoles("admin"))
     e.GET("/all", roleHandler.GetAllRole)
     e.GET("/:roleId", roleHandler.GetByIdRole)
-    e.PUT("/:roleId/edit", roleHandler.UpdateRole)
-    e.DELETE("/:roleId/delete", roleHandler.DeleteRole)
+    e.PUT("/:roleId/edit", roleHandler.UpdateRole, middleware.AllowRoles("admin"))
+    e.DELETE("/:roleId/delete", roleHandler.DeleteRole, middleware.AllowRoles("admin"))
 }
