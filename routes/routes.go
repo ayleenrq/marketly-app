ae23cef653683003811e6cc4ef23db068ab6f9c5
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2026-03-06 22:57:08 +0700

    update main route

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
-	"marketly-app/routes/admin_route"
-	"marketly-app/routes/role_route"
-	"marketly-app/routes/user_route"
+	admin_route "marketly-app/routes/admin_route"
+	category_route "marketly-app/routes/category_route"
+	role_route "marketly-app/routes/role_route"
+	seller_route "marketly-app/routes/seller_route"
+	user_route "marketly-app/routes/user_route"
 
 	"github.com/cloudinary/cloudinary-go/v2"
 	"github.com/labstack/echo/v4"
@@ -16,4 +18,6 @@ func Routes(e *echo.Echo, db *gorm.DB, cld *cloudinary.Cloudinary) {
 	role_route.RoleRoutes(v1.Group("/role"), db)
 	admin_route.AdminRoutes(v1.Group("/admin"), db)
 	user_route.UserRoutes(v1.Group("/user"), db, cld)
+	seller_route.SellerRoutes(v1.Group("/seller"), db, cld)
+	category_route.CategoryRoutes(v1.Group("/category"), db)
 }
