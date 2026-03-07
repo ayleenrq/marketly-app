fc7e72234b418f50d20d0ab56b2a0d0dcc56f44b
Ayleen Ruhul Qisthy <qisthyayleen@gmail.com>
2026-03-07 16:12:41 +0700

    update models

--- internal/models/order.go
+++ internal/models/order.go
@@ -3,11 +3,13 @@ package models
 import "time"
 
 type Order struct {
-	ID          int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	BuyerID     int       `gorm:"type:int;not null" json:"buyer_id"`
-	TotalAmount int       `gorm:"type:bigint" json:"total_amount"`
-	Status      string    `gorm:"type:varchar(50)" json:"status"`
-	Buyer       User      `gorm:"foreignKey:BuyerID" json:"buyer"`
-	CreatedAt   time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt   time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID          int         `gorm:"primaryKey;autoIncrement" json:"id"`
+	BuyerID     int         `gorm:"type:int;not null" json:"buyer_id"`
+	TotalAmount int         `gorm:"type:bigint" json:"total_amount"`
+	Status      string      `gorm:"type:varchar(50)" json:"status"`
+	User        User        `gorm:"foreignKey:BuyerID;references:ID" json:"buyer"`
+	OrderItems  []OrderItem `gorm:"foreignKey:OrderID" json:"items"`
+	Payment     *Payment    `gorm:"foreignKey:OrderID;references:ID" json:"payment"`
+	CreatedAt   time.Time   `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt   time.Time   `gorm:"autoUpdateTime" json:"updated_at"`
 }
--- internal/models/payment.go
+++ internal/models/payment.go
@@ -3,12 +3,17 @@ package models
 import "time"
 
 type Payment struct {
-	ID        int        `gorm:"primaryKey;autoIncrement" json:"id"`
-	OrderID   int        `gorm:"type:int;not null" json:"order_id"`
-	Method    string     `gorm:"type:varchar(50)" json:"method"`
-	Status    string     `gorm:"type:varchar(50)" json:"status"`
-	Amount    int        `gorm:"type:bigint;not null" json:"amount"`
-	PaidAt    *time.Time `json:"paid_at"`
-	Order     Order      `gorm:"foreignKey:OrderID"`
-	CreatedAt time.Time  `gorm:"autoCreateTime" json:"created_at"`
+	ID            int        `gorm:"primaryKey;autoIncrement" json:"id"`
+	OrderID       int        `gorm:"type:int;not null" json:"order_id"`
+	Method        string     `gorm:"type:varchar(50);not null" json:"method"` 
+	Status        string     `gorm:"type:varchar(50);not null" json:"status"`
+	Amount        int        `gorm:"type:bigint;not null" json:"amount"`
+	TransactionID string     `gorm:"type:varchar(255)" json:"transaction_id"` 
+	PaymentCode   string     `gorm:"type:varchar(255)" json:"payment_code"`   
+	PaymentURL    string     `gorm:"type:text" json:"payment_url"`            
+	PaidAt        *time.Time `json:"paid_at"`
+	ExpiredAt     *time.Time `json:"expired_at"`
+	Order         Order      `gorm:"foreignKey:OrderID"`
+	CreatedAt     time.Time  `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt     time.Time  `gorm:"autoUpdateTime" json:"updated_at"`
 }
